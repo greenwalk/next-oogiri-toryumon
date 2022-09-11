f49c71587b2aa2cabe10f22c79bb56a076ebc9be
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-09-11 23:54:35 +0900

    userモデルvalidation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :oogiris
   has_many :votes
   has_many :comments
 
   validates :name, presence: true
+  validates :oogiri_start, presence: true
 
   def admin_user?
     id == 1
