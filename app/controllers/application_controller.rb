6465aa9ffba135991a0b52a4090b7a9509ab9005
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-09-11 23:54:00 +0900

    deviseのストロングパラメータ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_now_field
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :oogiri_start])
+  end
   def set_now_field
     @now_field = Field.order(created_at: :desc).first
   end
