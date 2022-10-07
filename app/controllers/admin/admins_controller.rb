c509f95f82ac1297dd79afec42c6762d43056700
permy1225 <manentopa@gmail.com>
2022-10-07 22:24:31 +0900

    管理者ページ編集

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -2,7 +2,12 @@ class Admin::AdminsController < ApplicationController
   before_action :only_admin_user
 
   def top
+    # お題作成用
     @field = Field.new
+    # 過去のお題用
+    @fields_finished = Field.where(status: "finished")
+    # ユーザー一覧用
+    @users = User.order(created_at: :asc)
   end
 
   private
