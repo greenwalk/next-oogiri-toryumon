e8b0021cec84de67a22204853022d5ffafc798d9
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-10-08 16:17:53 +0900

    @now_field廃止&お題複数に対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :field_status_check
   before_action :set_now_field
 
   private
@@ -7,6 +8,22 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :oogiri_start])
   end
   def set_now_field
-    @now_field = Field&.order(created_at: :desc)&.first
+    @now_fields = if @field_status == "投稿"
+                    Field.status_posting
+                  elsif @field_status == "投票"
+                    Field.status_voting
+                  else
+                    Field.status_finished.limit(2)
+                  end
+  end
+
+  def field_status_check
+    if Field.status_posting.present?
+      @field_status = "投稿"
+    elsif Field.status_voting.present?
+      @field_status = "投票"
+    else
+      @field_status = "結果"
+    end
   end
 end
