d4c8633a56242d2f11b0a0ad3b0abea7cdcfe86e
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-10-08 16:18:30 +0900

    @now_field廃止&お題複数に対応

--- app/controllers/oogiris_controller.rb
+++ app/controllers/oogiris_controller.rb
@@ -1,7 +1,7 @@
 class OogirisController < ApplicationController
   before_action :authenticate_user!, except: [:index, :vote_show]
-  before_action :set_oogiri, only: [:edit, :update, :show, :vote_show, :destroy]
   before_action :set_field, only: [:index, :new, :create, :edit, :update, :show, :vote_show, :destroy]
+  before_action :set_oogiri, only: [:edit, :update, :show, :vote_show, :destroy]
   before_action :dont_look_result, only: [:index, :vote_show]
   before_action :dont_create_oogiri, only: [:new, :edit]
   before_action :dont_look_oogiri, only: [:show]
@@ -58,7 +58,7 @@ class OogirisController < ApplicationController
 
   private
   def oogiri_params
-    params.require(:oogiri).permit(:content, :point, :score, :get_rank).merge(user_id: current_user.id, field_id: @now_field.id)
+    params.require(:oogiri).permit(:content, :point, :score, :get_rank).merge(user_id: current_user.id, field_id: @field.id)
   end
 
   def set_oogiri
