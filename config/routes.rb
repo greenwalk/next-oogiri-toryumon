481bea432677d0e112c0f7ef944f933968138cb8
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-11-19 20:20:32 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,12 @@ Rails.application.routes.draw do
 
   # 滝壺
   resources :basin_fields, only:[:index] do
+    get '/basin_votes', to: 'basin_oogiris#vote'
     resources :basin_oogiris, only:[:index, :new, :create, :destroy] do
       resources :basin_likes, only:[:create, :destroy]
     end
   end
+  get 'basin_fields/now_basin_field', to: 'basin_fields#now_basin_field'
 
   resources :comments, only:[:index]
   get 'fields/now_field', to: 'fields#now_field_index'
