2db5b44647ba1a67577d3bc1e7998b07a67de214
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-11-25 23:49:36 +0900

    create table

--- db/migrate/20221125144424_create_toryu_settings.rb
+++ db/migrate/20221125144424_create_toryu_settings.rb
@@ -1,6 +1,8 @@
 class CreateToryuSettings < ActiveRecord::Migration[6.0]
   def change
     create_table :toryu_settings do |t|
+      t.integer :point, null: false, default: 0
+      t.integer :rank, null: false, default: 0
 
       t.timestamps
     end
