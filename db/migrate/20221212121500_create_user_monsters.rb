d1c0e8cfdc8950603cee1b23c714c8d6d066d116
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-12-16 23:37:55 +0900

    user_monstersテーブル作成

--- db/migrate/20221212121500_create_user_monsters.rb
+++ db/migrate/20221212121500_create_user_monsters.rb
@@ -1,6 +1,9 @@
 class CreateUserMonsters < ActiveRecord::Migration[6.0]
   def change
     create_table :user_monsters do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :monster, null: false, foreign_key: true
+      t.integer :status, null: false, default: 0
 
       t.timestamps
     end
