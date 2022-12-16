5e2488445a916786dadedd736105a3518710774f
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-12-17 03:09:32 +0900

    monsters生成用バッチ

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,5 +3,5 @@ class Monster < ApplicationRecord
   has_many :users, through: :user_monsters
 
   enum status: {unused: 0, used: 1 }, _prefix: true
-  enum species: {dragon: 0, human: 1, fish: 2, beast: 4, others: 5}, _prefix: true
+  enum species: {dragon: 0, human: 1, fish: 2, beast: 3, others: 4}, _prefix: true
 end
